d45607cc24d7d4c23f964e41e69f5fd71205da5d
awalariansyah <57579406+awalariansyah@users.noreply.github.com>
2021-02-17 15:33:38 +0700

    Add auto ok 'still watching' and fixed error

--- content.js
+++ content.js
@@ -3,7 +3,8 @@ console.log('Konichiwa')
 var tombolSkip = document.getElementsByClassName("ytp-ad-skip-button");
 var unskipIklan = document.getElementsByClassName('ytp-ad-text');
 var iklanBanner = document.getElementsByClassName('ytp-ad-overlay-close-button');
-var mute = document.getElementsByClassName('ytp-mute-button')[0]
+var mute = document.getElementsByClassName('ytp-mute-button')
+var menonton = document.getElementsByClassName('yt-simple-endpoint style-scope yt-button-renderer')
 
 setInterval( function(){
 	if (tombolSkip != undefined && tombolSkip.length > 0){
@@ -11,18 +12,23 @@ setInterval( function(){
 		console.log("Iklan terdeteksi");
 		tombolSkip[0].click();
 
-	}else if(unskipIklan != undefined && unskipIklan.length > 0){
+	}else if(unskipIklan != undefined && unskipIklan.length > 0 && mute.length > 0){
 
 		console.log('Unskippable Iklan terdeteksi');
-		if(mute.title == 'Mute (m)'){	
-			mute.click()
+		if(mute[0].title == 'Mute (m)'){	
+			mute[0].click()
 		}
 
 	}else{
-		if(mute.title == 'Unmute (m)'){	
-			mute.click()
+		if(mute != undefined && mute.length > 0){ 
+			if(mute[0].title == 'Unmute (m)'){	
+				mute[0].click()
+			}
 		}
+	}
 
+	if(menonton != undefined && menonton.length > 0){
+		menonton[1].click()
 	}
 
 	if (iklanBanner != undefined && iklanBanner.length > 0){
